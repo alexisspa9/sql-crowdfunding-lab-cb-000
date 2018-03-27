bbc92d28e501f80e1778ea425050c723f8b60c90
Alexander Spanos <alexisspa9@gmail.com>
2018-03-27 12:54:08 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -11,3 +11,9 @@ CREATE TABLE users(
   name TEXT,
   age INTEGER
 );
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
